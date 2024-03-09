4dede3c36b671fefca6ff9aefb1edf03888f15fa
Arman <armanulhaq10@gmail.com>
2024-03-09 17:00:32 +0530

    fix color

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -4,7 +4,7 @@ const Main = () => {
   return (
     <div className="main-image-container">
       <div className="main-text">
-        <h1>We Ensure better education for a better world</h1>
+        <h1>We ensure better education for a better world</h1>
         <p>
           Our cutting-edge curriculum is designed to empower students with the
           knowledge, skills, and experiences needed to excel in the dynamic
